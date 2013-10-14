70a700848f5a9c852ed5ab1ee5515107d5f087ce
neil hudson <neil.j.hudson@gmail.com>
2013-10-14 13:18:11 -0400

    created pattern class. working out how to store patterns and indicate adjustable elements and consequences

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@
 /*
  * GET home page.
  */
-
+var fs=require('fs');
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  //var file=fs.openSync("images/tshirt_scaled.svg",'r');
+  var file_contents=fs.readFileSync("public/images/tshirt_scaled.svg")
+  res.render('index', { title: 'Patternite' , svg: file_contents});
+
 };
